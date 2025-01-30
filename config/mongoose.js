80834b9a3cb3ba9cb6fc4b12321a702a425707b1
edgar <seo@disoin.com>
2025-01-30 16:50:34 -0600

    Se realiza ajuste a modulo de coneccion db a mongoose

--- config/mongoose.js
+++ config/mongoose.js
@@ -4,10 +4,7 @@ const dbConnect = async () => {
     const DB_URI = process.env.DB_URI; // Asegúrate de que esta variable esté definida
 
     try {
-        await mongoose.connect(DB_URI, { // Usa la variable de entorno
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        await mongoose.connect(DB_URI); // Usa la variable de entorno
         console.log('Conectado a MongoDB');
     } catch (err) {
         console.error('Error de conexión:', err);
